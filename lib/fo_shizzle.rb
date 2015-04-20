8b9c0467b9b3dd7bfe31fcff99ccdf1cc2c7c14a
Cory Ribson & Kim Dang <student@epicodus.com>
2015-04-20 16:53:17 -0700

    ignores the first letter of the word

--- lib/fo_shizzle.rb
+++ lib/fo_shizzle.rb
@@ -1,38 +1,31 @@
-#class String
-#  define_method(:fo_shizzle) do
-#    self.split("").each() do |letter|
-#      if letter == "s"
-#        letter = "z"
-#      end
-#    end
-#  end
-#end
-
-
-
-#class String
-#  define_method(:fo_shizzle) do
-#    self.split("").each() do |letter|
-#      if letter == "s"
-#        letter = "z"
-#      else
-#        letter = letter
-#      end
-#    end
-#  end
-#end
+# class String
+#   define_method(:fo_shizzle) do
+#     response = ""
+#     self.split("").each() do |letter|
+#       if letter == "s"
+#         letter = "z"
+#       else
+#           letter = letter
+#       end
+#       response = response.concat(letter)
+#     end
+#     response
+#   end
+# end
 
 
 class String
   define_method(:fo_shizzle) do
     response = ""
-    self.split("").each() do |letter|
+    split_word = self.split("")
+    response.concat(split_word.shift)
+    split_word.each() do |letter|
       if letter == "s"
-        letter = "z"
+        return_letter = "z"
       else
-          letter = letter
+        return_letter = letter
       end
-      response = response.concat(letter)
+      response.concat(return_letter)
     end
     response
   end
--- spec/fo_shizzle_spec.rb
+++ spec/fo_shizzle_spec.rb
@@ -1,10 +1,13 @@
 require('rspec')
 require('fo_shizzle')
 
+
 describe('String#fo_shizzle') do
   it("splits a string into letters and changes s to z") do
     expect("dogs rule".fo_shizzle()).to(eq("dogz rule"))
   end
 
-
+  it("doesn't change the s if it is the first letter of the word") do
+    expect("set ups".fo_shizzle()).to(eq("set upz"))
+  end
 end
