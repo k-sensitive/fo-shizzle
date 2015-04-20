e17c119fb34c0c812572914beef86f7f8c8e3500
Cory Ribson & Kim Dang <student@epicodus.com>
2015-04-20 16:22:56 -0700

    Concats to the original string with s changes

--- lib/fo_shizzle.rb
+++ lib/fo_shizzle.rb
@@ -1,9 +1,39 @@
+#class String
+#  define_method(:fo_shizzle) do
+#    self.split("").each() do |letter|
+#      if letter == "s"
+#        letter = "z"
+#      end
+#    end
+#  end
+#end
+
+
+
+#class String
+#  define_method(:fo_shizzle) do
+#    self.split("").each() do |letter|
+#      if letter == "s"
+#        letter = "z"
+#      else
+#        letter = letter
+#      end
+#    end
+#  end
+#end
+
+
 class String
   define_method(:fo_shizzle) do
+    response = ""
     self.split("").each() do |letter|
       if letter == "s"
         letter = "z"
+      else
+          letter = letter
       end
+      response = response.concat(letter)
     end
+    response
   end
 end
--- spec/fo_shizzle_spec.rb
+++ spec/fo_shizzle_spec.rb
@@ -3,8 +3,8 @@ require('fo_shizzle')
 
 describe('String#fo_shizzle') do
   it("splits a string into letters and changes s to z") do
-    expect("dogs rule".fo_shizzle()).to(eq(["d", "o", "g", "z", " ", "r", "u", "l", "e"]))
+    expect("dogs rule".fo_shizzle()).to(eq("dogz rule"))
   end
 
-  
+
 end
